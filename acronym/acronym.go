6648e27d92a568dccbee79531a518aa6fa169e37
trad3r <c1mba@yandex.ru>
2020-05-03 09:43:07 +0300

    Окончен acronym

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -1,10 +1,28 @@
 package acronym
 
-import "strings"
+import (
+	"regexp"
+	"unicode"
+)
 
 // Abbreviate convert a phrase to its acronym.
 func Abbreviate(s string) string {
-	words := strings.Split(s, " ")
+	split := regexp.MustCompile(`\s|-`)
+	words := split.Split(s, -1)
+	acronym := []rune{}
 
-	return ""
+	for _, word := range words {
+		if len(word) == 0 {
+			continue
+		}
+		chars := []rune(word)
+		for _, char := range chars {
+			if unicode.IsLetter(char) {
+				acronym = append(acronym, unicode.ToUpper(char))
+				break
+			}
+		}
+	}
+
+	return string(acronym)
 }
