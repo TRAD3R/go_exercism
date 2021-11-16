9da6d7171047a16e1996defa57081e430a2f17c0
trad3r <tatusr@gmail.com>
2021-11-16 11:29:01 +0300

    Parallel letter версия с WaitGroup

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,5 +1,7 @@
 package letter
 
+import "sync"
+
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
@@ -15,22 +17,51 @@ func Frequency(s string) FreqMap {
 
 // ConcurrentFrequency counts the frequency of each rune in the given strings,
 // by making use of concurrency.
+//func ConcurrentFrequency(l []string) FreqMap {
+//	rchan := make(chan FreqMap)
+//	freqMap := FreqMap{}
+//
+//	for _, line := range l {
+//		go func(line string, rchan chan<- FreqMap) {
+//
+//			rchan <- Frequency(line)
+//
+//		}(line, rchan)
+//	}
+//
+//	for i := 0; i < len(l); i++ {
+//		for key, value := range <-rchan {
+//			freqMap[key] += value
+//
+//		}
+//	}
+//
+//	return freqMap
+//}
+
 func ConcurrentFrequency(l []string) FreqMap {
-	rchan := make(chan FreqMap)
+	rchan := make(chan FreqMap, len(l))
 	freqMap := FreqMap{}
+	wg := sync.WaitGroup{}
+
+	wg.Add(len(l))
+
+	go func() {
+		wg.Wait()
+		close(rchan)
+	}()
 
 	for _, line := range l {
 		go func(line string, rchan chan<- FreqMap) {
-
+			defer wg.Done()
 			rchan <- Frequency(line)
 
 		}(line, rchan)
 	}
 
-	for i := 0; i < len(l); i++ {
-		for key, value := range <-rchan {
+	for m := range rchan {
+		for key, value := range m {
 			freqMap[key] += value
-
 		}
 	}
 
