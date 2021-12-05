ed106a897097e8712568e6775ec47460e6c45c77
trad3r <tatusr@gmail.com>
2021-12-05 05:34:19 +0300

    Tree building

---
+++ tree-building/tree_building.go
@@ -0,0 +1,38 @@
+package tree
+
+import (
+	"errors"
+	"sort"
+)
+
+// Define the Record type
+type Record struct {
+	ID     int
+	Parent int
+}
+
+// Define the Node type
+type Node struct {
+	ID       int
+	Children []*Node
+}
+
+func Build(records []Record) (*Node, error) {
+	sort.Slice(records, func(cur, prev int) bool {
+		return records[cur].ID < records[prev].ID
+	})
+
+	nodes := map[int]*Node{}
+	for i, r := range records {
+		if i != r.ID || r.ID < r.Parent || (r.ID > 0 && r.ID == r.Parent) {
+			return nil, errors.New("Invalid records structure")
+		}
+
+		nodes[r.ID] = &Node{ID: r.ID}
+		if r.ID != 0 {
+			par := nodes[r.Parent]
+			par.Children = append(par.Children, nodes[r.ID])
+		}
+	}
+	return nodes[0], nil
+}
