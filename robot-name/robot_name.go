fb081ee6a10b8fa1d3fd3330ad86a17b343f549c
trad3r <tatusr@gmail.com>
2021-12-23 14:49:29 +0300

    удалил лишние методы и константы

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -6,12 +6,6 @@ import (
 	"time"
 )
 
-const (
-	maxRandNames = 26 * 26 * 10 * 10 * 10
-	letterMax    = 26
-	letterShift  = 65
-)
-
 var (
 	random       = rand.New(rand.NewSource(time.Now().UnixNano()))
 	existedNames = map[string]bool{}
@@ -27,7 +21,7 @@ func (r *Robot) Name() (string, error) {
 		return r.name, nil
 	}
 
-	if len(existedNames) == maxRandNames {
+	if len(existedNames) == 26*26*10*10*10 {
 		return "", fmt.Errorf("Out of names")
 	}
 
@@ -46,22 +40,8 @@ func (r *Robot) Reset() {
 }
 
 func newName() string {
-	return fmt.Sprintf(
-		"%s%s%d%d%d",
-		randomLetter(),
-		randomLetter(),
-		randomInt(),
-		randomInt(),
-		randomInt(),
-	)
-}
-
-func randomLetter() string {
-	letter := random.Intn(letterMax) + letterShift
-
-	return string(rune(letter))
-}
-
-func randomInt() int {
-	return random.Intn(10)
+	r1 := random.Intn(26) + 'A'
+	r2 := random.Intn(26) + 'A'
+	num := random.Intn(1000)
+	return fmt.Sprintf("%c%c%03d", r1, r2, num)
 }
